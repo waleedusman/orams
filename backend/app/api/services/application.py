65982ec9d6a2b017e5395c3fdb6176bc97b74273
Matt <matt.smith@digital.gov.au>
2018-05-16 16:17:28 +1000

    more pruning

--- backend/app/api/services/application.py
+++
@@ -1,9 +0,0 @@
-from app.api.helpers import Service
-from app.models import Application
-
-
-class ApplicationService(Service):
-    __model__ = Application
-
-    def __init__(self, *args, **kwargs):
-        super(ApplicationService, self).__init__(*args, **kwargs)
