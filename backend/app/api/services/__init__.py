ab7dbe1861c310711197ac3c599336ace05a165e
Bryan Signey <bsigney@dius.com.au>
2018-05-18 12:15:57 +1000

    MAR-1774: removed unnecessary marketplace service from api services startup

--- backend/app/api/services/__init__.py
+++ backend/app/api/services/__init__.py
@@ -1,24 +1,12 @@
 from .prices import PricesService
-from .application import ApplicationService
 from .audit import AuditService, AuditTypes
 from .assessments import AssessmentsService
-from .domain import DomainService
-from .briefs import BriefsService
 from .suppliers import SuppliersService
-from .lots import LotsService
-from .brief_overview import BriefOverviewService
-from .brief_responses import BriefResponsesService
 from .users import UsersService
 
 prices = PricesService()
-application_service = ApplicationService()
 audit_service = AuditService()
 audit_types = AuditTypes
 assessments = AssessmentsService()
-domain_service = DomainService()
-briefs = BriefsService()
 suppliers = SuppliersService()
-lots_service = LotsService()
-brief_overview_service = BriefOverviewService()
-brief_responses_service = BriefResponsesService()
 users = UsersService()
