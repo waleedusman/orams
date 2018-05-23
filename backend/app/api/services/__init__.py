77426cd073b0a9fb6aa39455160e0a65c5fda02b
Bryan Signey <bsigney@dius.com.au>
2018-05-24 09:13:19 +1000

    AOP2-1: fixed broken backend tests

--- backend/app/api/services/__init__.py
+++ backend/app/api/services/__init__.py
@@ -2,6 +2,7 @@ from .prices import PricesService
 from .audit import AuditService, AuditTypes
 from .assessments import AssessmentsService
 from .suppliers import SuppliersService
+from .brief_responses import BriefResponsesService
 from .users import UsersService
 
 prices = PricesService()
@@ -9,4 +10,5 @@ audit_service = AuditService()
 audit_types = AuditTypes
 assessments = AssessmentsService()
 suppliers = SuppliersService()
+brief_responses_service = BriefResponsesService()
 users = UsersService()
