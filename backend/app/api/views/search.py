fbaa0db8556fc829fc905dff48b986a67fd818c8
Bryan Signey <bsigney@dius.com.au>
2018-06-05 08:54:50 +1000

    AOP2-48: updated swagger doc for search suppliers

--- backend/app/api/views/search.py
+++ backend/app/api/views/search.py
@@ -16,6 +16,100 @@ from ...models import Supplier, SupplierFramework, SupplierContact, Contact
 @login_required
 @role_required('admin')
 def find_suppliers():
+    """Find suppliers by name prefix or email (role=admin)
+    ---
+    tags:
+      - search
+    security:
+      - basicAuth: []
+    parameters:
+      - name: supplier_name_prefix
+        in: path
+        type: string
+        required: true
+    definitions:
+      SupplierService:
+        type: object
+        properties:
+          id:
+            type: integer
+          name:
+            type: string
+          subCategories:
+            type: array
+            items:
+              $ref: '#/definitions/SupplierCategory'
+      SupplierCategory:
+        type: object
+        properties:
+          id:
+            type: integer
+          name:
+            type: string
+      Supplier:
+        type: object
+        properties:
+          abn:
+            type: string
+          address_address_line:
+            type: string
+          address_country:
+            type: string
+          address_postal_code:
+            type: string
+          address_state:
+            type: string
+          address_suburb:
+            type: string
+          category_name:
+            type: string
+          code:
+            type: string
+          contact_email:
+            type: string
+          contact_name:
+            type: string
+          contact_phone:
+            type: string
+          email:
+            type: string
+          id:
+            type: number
+          linkedin:
+            type: string
+          name:
+            type: string
+          phone:
+            type: string
+          regions:
+            type: array
+            items:
+                type: object
+                properties:
+                  name:
+                    type: string
+                  state:
+                    type: string
+          representative:
+            type: string
+          services:
+            type: array
+            items:
+                $ref: '#/definitions/SupplierService'
+          summary:
+            type: string
+          website:
+            type: string
+    responses:
+      200:
+        description: List of suppliers starting with prefix
+        type: object
+        properties:
+            suppliers:
+                type: array
+                items:
+                    $ref: '#/definitions/Supplier'
+    """
     prefix = request.args.get('supplier_name_prefix')
     supplier_data = suppliers.find_suppliers(prefix)
 
