d392391011200938a1e6617116b3775c61b80d31
Bryan Signey <bsigney@dius.com.au>
2018-06-04 10:30:42 +1000

    AOP2-41: refactored find suppliers into a service

--- backend/app/api/services/suppliers.py
+++ backend/app/api/services/suppliers.py
@@ -1,5 +1,6 @@
+from sqlalchemy.sql import or_
 from app.api.helpers import Service
-from app.models import Supplier
+from app.models import Supplier, SupplierFramework, SupplierContact, Contact
 
 
 class SuppliersService(Service):
@@ -7,3 +8,25 @@ class SuppliersService(Service):
 
     def __init__(self, *args, **kwargs):
         super(SuppliersService, self).__init__(*args, **kwargs)
+
+    def find_suppliers(self, prefix):
+        orams_framework_id = 8
+
+        suppliers = Supplier.query.join(Supplier.frameworks) \
+            .filter(SupplierFramework.framework_id == orams_framework_id) \
+            .filter(Supplier.abn.is_(None) | (Supplier.abn != Supplier.DUMMY_ABN)) \
+            .filter(Supplier.status != 'deleted')
+
+        if prefix:
+            suppliers = suppliers.outerjoin(SupplierContact).outerjoin(Contact)
+            # case insensitive LIKE comparison for matching supplier names, supplier email and contact email
+            suppliers = suppliers.filter(or_(
+                Supplier.name.ilike(prefix + '%'),
+                Supplier.data['email'].astext.ilike('%{}%'.format(prefix)),
+                Contact.email.ilike('%{}%'.format(prefix))
+                ))
+
+        suppliers = suppliers.distinct(Supplier.name, Supplier.code)\
+            .order_by(Supplier.name)
+
+        return [supplier.serializable for supplier in suppliers.all()]
--- backend/app/api/views/search.py
+++ backend/app/api/views/search.py
@@ -2,14 +2,13 @@ from flask import (
     abort, current_app, jsonify, request, url_for
 )
 from flask_login import login_required
-from sqlalchemy.exc import DataError
-from sqlalchemy.sql import or_
 from app.api import api
 from app.utils import (
     get_json_from_request, get_positive_int_or_400, get_valid_page_or_1, pagination_links
 )
 # from app.api.suppliers import get_supplier, update_supplier, list_suppliers
 from app.api.helpers import role_required
+from app.api.services import suppliers
 from ...models import Supplier, SupplierFramework, SupplierContact, Contact
 
 
@@ -17,26 +16,7 @@ from ...models import Supplier, SupplierFramework, SupplierContact, Contact
 @login_required
 @role_required('admin')
 def find_suppliers():
-    orams_framework_id = 8
     prefix = request.args.get('supplier_name_prefix')
-
-    suppliers = Supplier.query.join(Supplier.frameworks) \
-        .filter(SupplierFramework.framework_id == orams_framework_id) \
-        .filter(Supplier.abn.is_(None) | (Supplier.abn != Supplier.DUMMY_ABN)) \
-        .filter(Supplier.status != 'deleted')
-
-    if prefix:
-        suppliers = suppliers.outerjoin(SupplierContact).outerjoin(Contact)
-        # case insensitive LIKE comparison for matching supplier names, supplier email and contact email
-        suppliers = suppliers.filter(or_(
-            Supplier.name.ilike(prefix + '%'),
-            Supplier.data['email'].astext.ilike('%{}%'.format(prefix)),
-            Contact.email.ilike('%{}%'.format(prefix))
-            ))
-
-    suppliers = suppliers.distinct(Supplier.name, Supplier.code)\
-        .order_by(Supplier.name)
-
-    supplier_data = [supplier.serializable for supplier in suppliers.all()]
+    supplier_data = suppliers.find_suppliers(prefix)
 
     return jsonify(suppliers=supplier_data)
